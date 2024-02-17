88f0e49025d50f6abcb1a7c0ff320940977e8262
Tim Roberts <timrobertsusa@pluralpoint.com>
2024-02-17 08:06:18 -0500

    Update post-creation.sh
    changed location. of log file- to show in workspaces

--- .devcontainer/post-creation.sh
+++ .devcontainer/post-creation.sh
@@ -1,21 +1,23 @@
-printf "Start of Script File\n" > /workspaces/BigLog.txt
+#REPO_NAME=$(basename "$(git remote get-url origin)" .git)
+#printf "Start of Script File\n" > /workspaces/BigLog.txt
+REPO_NAME=$(basename "$(git remote get-url origin)" .git)
+printf "Start Log Codespace build $REPO_NAME\n" > /workspaces/$REPO_NAME/CSBuildLog.txt
 
-printf "BEFORE CD\n" >> /workspaces/BigLog.txt
+################
+printf "BEFORE CD\n" >> /workspaces/$REPO_NAME/CSBuildLog.txt
 cd /usr/share
 
 mv dotnet dotnet-8.0.101
 mkdir dotnet
 
-printf "BEFORE curl command\n" >> /workspaces/BigLog.txt
+printf "BEFORE curl command\n" >> /workspaces/$REPO_NAME/CSBuildLog.txt
 curl -Lo dotnet.tar.gz https://download.visualstudio.microsoft.com/download/pr/85bcc525-4e9c-471e-9c1d-96259aa1a315/930833ef34f66fe9ee2643b0ba21621a/dotnet-sdk-8.0.201-linux-x64.tar.gz
 
-printf "BEFORE tar command\n" >> /workspaces/BigLog.txt
+printf "BEFORE tar command\n" /workspaces/$REPO_NAME/CSBuildLog.txt
 tar -C dotnet -xf dotnet.tar.gz
 
-printf "BEFORE remove file\n" >> /workspaces/BigLog.txt
+printf "BEFORE remove file\n" >> /workspaces/$REPO_NAME/CSBuildLog.txt
 rm dotnet.tar.gz
 
-printf "End of Script File\n" >> /workspaces/BigLog.txt
+printf "End of Build Script File\n" >> /workspaces/$REPO_NAME/CSBuildLog.txt
 ################
-REPO_NAME=$(basename "$(git remote get-url origin)" .git)
-printf "Repo Name: $REPO_NAME\n" >> /workspaces/BigLog.txt
